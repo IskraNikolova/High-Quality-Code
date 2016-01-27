72a251a70b1a8ef056964c35dcbfc72d444dec15
IskraNikolova <air_iskra@yahoo.com>
2016-01-27 14:18:26 +0200

    add a new folder

---
+++ HomeworkSOLIDPrinciples-In-SoftwareDesign/LoggerLibrary/Layouts/SimpleLayout.cs
@@ -0,0 +1,13 @@
+﻿namespace LoggerLibrary.Layouts
+{
+    using System;
+    using Interfaces;
+
+    public class SimpleLayout : ILayout
+    {
+        public string Format(string msg, ReportLevel level, DateTime date)
+        {
+            return $"{date} - {level} - {msg}";
+        }
+    }
+}
