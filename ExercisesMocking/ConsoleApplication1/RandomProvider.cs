1d68c11579ade4040d481b39cf2ef524b9e9c4a4
IskraNikolova <air_iskra@yahoo.com>
2016-02-01 15:49:33 +0200

    update

---
+++ ExercisesMocking/ConsoleApplication1/RandomProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class RandomProvider: IRandomProvider
+    {
+        private Random random;
+
+        public RandomProvider()
+        {
+            this.random = new Random();
+        }
+
+        public int GetRandomNumber()
+        {
+            return random.Next();
+        }
+    }
+}
