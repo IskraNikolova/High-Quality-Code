be7e4b79bbc7b55cb7d63332e5d1b706eeed1f62
IskraNikolova <air_iskra@yahoo.com>
2016-03-02 12:14:38 +0200

    add interface IFactory

---
+++ DesignPatterns/SimpleFactoryDemo/Interfaces/IFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleFactoryDemo.Interfaces
+{
+    public interface IFactory
+    {
+        IStudent Create(string type, string name, IList<double> evaluations);
+    }
+}
